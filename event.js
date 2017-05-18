cf85769496fd95227579fc61e658b732c5081f86
tigranbarseghyan <tigran.barsegyan@gmail.com>
2017-05-18 20:50:59 +0400

    Some new operations with processes

--- event.js
+++ event.js
@@ -1,8 +1,20 @@
 'use strict';
 
+let counter = 0;
+
 //console.log(process.env);
-setInterval(function(){
-  console.log('Hello with delay');
+
+//Will terminate the infinite loop when counter is equal to 2
+const k = setInterval(function(){
+  console.log(`Called ${++counter} times`);
+  if(counter === 2){
+    clearInterval(k);
+  }
 }, 3000)
+
+//Will wait to cin some info, it will parse into uppercase
+process.stdin.on('data', function(d){
+ console.log(d.toString().toUpperCase());
+})
 
 //Callbacks
