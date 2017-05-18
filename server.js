b8130509f6d8438cc3900a9f8758a8c1a392b92f
tigranbarseghyan <tigran.barsegyan@gmail.com>
2017-05-18 21:05:38 +0400

    The first server with nodejs

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/'){
+    res.setHeader('content-type', 'text/html');
+    res.end(
+      `
+      <!doctype html>
+      <body>
+      <h1>Some header!</h1>
+      <p>What a body =P</p>
+      </body>
+      `)
+  } else {
+    res.end('UNKNOWN');
+  }
+})
+
+server.listen(8080, () => console.log('Started our server'));
