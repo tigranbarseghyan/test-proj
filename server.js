e78f981a1913207281ee493f9216918067bebbe0
tigranbarseghyan <tigran.barsegyan@gmail.com>
2017-05-19 18:42:43 +0400

    Call function in ports.js file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const http = require('http');
+const http = require('http'); //download http module. This module is from node
+                                // If we use some passwords and bank accounts, so we use https module
+
+const p = require('./ports');
 
 const server = http.createServer((req, res) => {
   if(req.url === '/'){
@@ -9,7 +12,7 @@ const server = http.createServer((req, res) => {
       `
       <!doctype html>
       <body>
-      <h1>Some header!</h1>
+      <h1> ${p.upper(__dirname)}</h1>
       <p>What a body =P</p>
       </body>
       `)
