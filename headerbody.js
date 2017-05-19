38eeeb3379b4f5cad4b2b6f0fb91519066110eed
tigranbarseghyan <tigran.barsegyan@gmail.com>
2017-05-19 21:11:57 +0400

    Updated the function to return the object with head and body keys

--- headerbody.js
+++ headerbody.js
@@ -2,9 +2,20 @@
 
 const retVal = (myList) => {
   const data = myList.reduce((prev, total) => prev + total);
-  console.log(data.split('\r\n\r\n')[0]);
-  console.log('EXAV');
-  console.log(data.split('\r\n\r\n')[1]);
+  const headerpart = data.split('\r\n\r\n')[0];
+  const bodypart = data.split('\r\n\r\n')[1];
+  const retObj = {};
+  const headArray = [];
+  const bodyArray = [];
+  for (const i of headerpart.split('\r\n')){
+    headArray.push(i);
+  }
+  for (const i of bodypart.split('\r\n')){
+    bodyArray.push(i);
+  }
+  retObj['head'] = headArray;
+  retObj['body'] = bodyArray;
+  return retObj;
 }
 
 module.exports = {
