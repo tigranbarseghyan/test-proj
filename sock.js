407e91f2d0e80c8a8b2e7ef8f87d626b834132e1
tigranbarseghyan <tigran.barsegyan@gmail.com>
2017-05-19 21:01:49 +0400

    Move the logic of the data function to headerbody file

---
+++ sock.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const net = require('net');
+const sock = new net.Socket;
+
+const k = require('./headerbody');
+
+sock.on('connect', () => {
+  sock.write('GET /en/ HTTP/1.1 \r\n');
+  sock.write('Host istc.am');
+  sock.write('\r\n\r\n');
+});
+
+sock.setEncoding('utf-8');
+const total_data = [];
+sock.on('data', d => total_data.push(d));
+sock.on('end', () => k.retVal(total_data));
+
+sock.connect(
+  {
+    port : 80,
+    host : 'istc.am'
+  }
+);
